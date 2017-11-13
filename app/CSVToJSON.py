01b03b6dc44c6a37b7ed9f7ea71d97f1d8f0e260
wcabynessa <wcabynessa@github.com>
2017-11-14 01:23:53 +0800

    Add load_order_order_line_data, load_item_data

--- app/CSVToJSON.py
+++ app/CSVToJSON.py
@@ -3,6 +3,7 @@ import csv
 import itertools
 import json
 
+from collections import deque
 from StatsCollector import StatsCollector
 
 class CSVToJSON:
@@ -10,11 +11,13 @@ class CSVToJSON:
     DEFAULT_DIRECTORY = "."
     DEFAULT_ROW_READ = 1000000000
     DEFAULT_OUT_DIR = "data"
+    SEPARATOR = ","
+    JOIN_CH = "-"
 
     def __init__(self, dir_path = DEFAULT_DIRECTORY, row_count = DEFAULT_ROW_READ, out_dir = DEFAULT_OUT_DIR):
-        self.OUT_DIR = out_dir
+        self.OUT_DIR = out_dir if out_dir else DEFAULT_OUT_DIR
         self.DIR_PATH = dir_path
-        self.ROW_COUNT = row_count
+        self.ROW_COUNT = row_count if row_count else self.DEFAULT_ROW_READ
 
         self.WAREHOUSE_FILE_PATH  = self.DIR_PATH + "/warehouse.csv"
         self.ORDER_FILE_PATH      = self.DIR_PATH + "/order.csv"
@@ -24,17 +27,16 @@ class CSVToJSON:
         self.ORDER_LINE_FILE_PATH = self.DIR_PATH + "/order-line.csv"
         self.STOCK_FILE_PATH      = self.DIR_PATH + "/stock.csv"
 
-        self.OUT_WAREHOUSE_FILE_PATH  = self.OUT_DIR + "/warehouse.csv"
-        self.OUT_ORDER_FILE_PATH      = self.OUT_DIR + "/order.csv"
-        self.OUT_DISTRICT_FILE_PATH   = self.OUT_DIR + "/district.csv"
-        self.OUT_CUSTOMER_FILE_PATH   = self.OUT_DIR + "/customer.csv"
-        self.OUT_ITEM_FILE_PATH       = self.OUT_DIR + "/item.csv"
-        self.OUT_ORDER_LINE_FILE_PATH = self.OUT_DIR + "/order-line.csv"
-        self.OUT_STOCK_FILE_PATH      = self.OUT_DIR + "/stock.csv"
-        self.OUT_WAREHOUSE_TAX_FILE_PATH = self.OUT_DIR + "/warehouse-tax.csv"
-        self.OUT_DISTRICT_NEXT_ORDER_ID  = self.OUT_DIR + "/district-next-order-id.csv"
+        self.OUT_WAREHOUSE_FILE_PATH  = self.OUT_DIR + "/warehouse.json"
+        self.OUT_DISTRICT_FILE_PATH   = self.OUT_DIR + "/district.json"
+        self.OUT_CUSTOMER_FILE_PATH   = self.OUT_DIR + "/customer.json"
+        self.OUT_ITEM_FILE_PATH       = self.OUT_DIR + "/item.json"
+        self.OUT_ORDER_ORDER_LINE_FILE_PATH = self.OUT_DIR + "/order-order-line.json"
+        self.OUT_STOCK_FILE_PATH      = self.OUT_DIR + "/stock.json"
+        self.OUT_WAREHOUSE_TAX_FILE_PATH = self.OUT_DIR + "/warehouse-tax.json"
+        self.OUT_DISTRICT_NEXT_ORDER_ID  = self.OUT_DIR + "/district-next-order-id.json"
         self.OUT_DISTRICT_NEXT_SMALLEST_ORDER_ID_DATA =           \
-            self.OUT_DIR + "/district-next-smallest-order-id.csv"
+            self.OUT_DIR + "/district-next-smallest-order-id.json"
 
 
     def timemeasure(original_function):
@@ -79,6 +81,7 @@ class CSVToJSON:
                              'd_ytd'     : line[9]})
         json.dump(out_data, out_file)
 
+
     @timemeasure
     def load_customer_data(self, csv_file, out_file):
         reader = csv.reader(csv_file)
@@ -105,10 +108,122 @@ class CSVToJSON:
                              'c_payment_cnt' : line[18],
                              'c_delivery_cnt': line[19],
                              'c_data'        : line[20]});
+            c_w_id, c_d_id, c_id, c_first, c_middle, c_last =            \
+                    line[0], line[1], line[2], line[3], line[4], line[5]
+            self.c_map[self.JOIN_CH.join([c_w_id, c_d_id, c_id])] = (c_first, c_middle, c_last)
+
         json.dump(out_data, out_file)
+
+
+    @timemeasure
+    def load_item_data(self, csv_file, out_file):
+        reader = csv.reader(csv_file)
+        out_data = []
+        for line in itertools.islice(reader, self.ROW_COUNT):
+            out_data.append({'i_id'   : line[0],
+                             'i_name' : line[1],
+                             'i_price': line[2],
+                             'i_im_id': line[3],
+                             'i_data' : line[4]})
+            # Store i_name for order_order_line
+            i_id, i_name = line[0], line[1]
+            self.i_map[i_id] = i_name
+        json.dump(out_data, out_file)
+
+
+    def read_another_orderline(self, current_order, order_line_file):
+        # Reading new orderline
+        line = order_line_file.readline()
+        if line:
+            line = line[:-1]
+            line = line.split(self.SEPARATOR)
+            self.order_line_queue.append({'ol_w_id'       :line[0],
+                                          'ol_d_id'       :line[1],
+                                          'ol_o_id'       :line[2],
+                                          'ol_number'     :line[3],
+                                          'ol_i_id'       :line[4],
+                                          'ol_delivery_d' :line[5],
+                                          'ol_amount'     :line[6],
+                                          'ol_supply_w_id':line[7],
+                                          'ol_quantity'   :line[8],
+                                          'ol_dist_info'  :line[9]});
+
+        while (len(self.order_line_queue) > 0):
+            order_line = self.order_line_queue[0]
+            key = (int(order_line['ol_w_id']), int(order_line['ol_d_id']), int(order_line['ol_o_id']))
+
+            if (current_order < key):
+                return None
+            elif (current_order == key):
+                self.order_line_queue.popleft()
+                return order_line
+            else:
+                self.order_line_queue.popleft()
+
+        return None
+
+
+
+    @timemeasure
+    def load_order_orderline_data(self, order_file, order_line_file, out_file):
+        # Opening list bracket
+        out_file.write("[");
+
+        # Assume that order_file is sorted in ascending order of (w_id, d_id, o_id)
+        # Assume that order_line_file is sorted in ascending order of (w_id, d_id, o_id, o_number)
+        line_count = self.ROW_COUNT
+        while (line_count > 0):
+            line = order_file.readline()
+            if not line:
+                break
+            line_count -= 1
+
+            # Remove new line char at the end of string
+            line = line[:-1]
+            line = line.split(self.SEPARATOR)
+
+            o_w_id, o_d_id, o_id, o_c_id = line[0], line[1], line[2], line[3]
+            c_first, c_middle, c_last = self.c_map[self.JOIN_CH.join([o_w_id, o_d_id, o_c_id])]
+
+            obj = {'o_w_id'      : line[0],
+                   'o_d_id'      : line[1],
+                   'o_id'        : line[2],
+                   'o_c_id'      : line[3],
+                   'o_carrier_id': line[4],
+                   'o_ol_cnt'    : line[5],
+                   'o_all_local' : line[6],
+                   'o_entry_d'   : line[7],
+                   'o_c_first'   : c_first,
+                   'o_c_middle'  : c_middle,
+                   'o_c_last'    : c_last,
+                   'o_delivery_d': None,
+                   'o_orderlines': []}
+
+            # Reading order_line match this current order
+            while (True):
+                current_order = (int(o_w_id), int(o_d_id), int(o_id))
+                order_line_obj = self.read_another_orderline(current_order, order_line_file)
+
+                if not order_line_obj:
+                    break;
+
+                obj['o_delivery_d'] = order_line_obj['ol_delivery_d']
+                del order_line_obj['ol_delivery_d']
+
+                ol_i_id = order_line_obj['ol_i_id']
+                order_line_obj['i_name'] = self.i_map[ol_i_id]
+                obj['o_orderlines'].append(order_line_obj)
+
+            json.dump(obj, out_file)
+
+        # Closing list bracket
+        out_file.write("]");
+
 
     def execute(self):
         # Initialize map for denormalizing tables
+        self.i_map, self.c_map = {}, {}
+        self.order_line_queue = deque()
 
         self.load_warehouse_data(
                 open(self.WAREHOUSE_FILE_PATH),
@@ -119,21 +234,29 @@ class CSVToJSON:
         self.load_customer_data(
                 open(self.CUSTOMER_FILE_PATH),
                 open(self.OUT_CUSTOMER_FILE_PATH, "w"))
-    
+        self.load_item_data(
+                open(self.ITEM_FILE_PATH),
+                open(self.OUT_ITEM_FILE_PATH, "w"))
+        # Reset outfile before writing, Append each order to the file so that we don't have
+        # to store in memory
+        open(self.OUT_ORDER_ORDER_LINE_FILE_PATH, "w").close()
+        self.load_order_orderline_data(
+               open(self.ORDER_FILE_PATH),
+               open(self.ORDER_LINE_FILE_PATH),
+               open(self.OUT_ORDER_ORDER_LINE_FILE_PATH, "a"))
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     # List of arguments
     ap.add_argument("-p", "--path", required=True, help="Path to directory containing data")
     ap.add_argument("-c", "--count", required=False, help="Number of rows to be read")
-    ap.add_argument("-o", "--output", required=True, help="Output directory")
+    ap.add_argument("-o", "--output", required=False, help="Output directory")
     args = vars(ap.parse_args())
 
     # Getting arguments
     dir_path = args['path']
     row_count = int(args['count']) if args['count'] else None
     out_dir = args['output']
-    print row_count
 
     converter = CSVToJSON(dir_path, row_count, out_dir)
     converter.execute()
