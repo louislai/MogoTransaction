844c59bca174bdbf65417556fcd97b87927d78e0
Louis Lai <hoangdung2506@gmail.com>
2017-11-16 21:39:08 +0800

    update setup scripts

--- shard.sh
+++ shard.sh
@@ -3,13 +3,13 @@
 mongo --port 21100 <<EOF
 use cs4224;
 
-db.shardCollection("cs4224.district-next-order-id", { d_w_id: 1, d_id: 1 });
-db.shardCollection("cs4224.warehouse-tax", { w_id: "hashed" });
-db.shardCollection("cs4224.warehouse", { w_id: "hashed" });
-db.shardCollection("cs4224.district", { d_w_id: 1, d_id: 1 });
-db.shardCollection("cs4224.district-next-undelivered-id", { d_w_id: 1, d_id: 1 });
-db.shardCollection("cs4224.customer", { c_w_id: 1, c_d_id: 1, c_id: 1 });
-db.shardCollection("cs4224.order-order-line", { o_w_id: 1, o_d_id: 1, o_id: -1 });
-db.shardCollection("cs4224.item", { i_id: "hashed" });
-db.shardCollection("cs4224.stock", { s_w_id: 1, s_i_id: 1 });
+sh.shardCollection("cs4224.district-next-order-id", { d_w_id: 1, d_id: 1 });
+sh.shardCollection("cs4224.warehouse-tax", { w_id: "hashed" });
+sh.shardCollection("cs4224.warehouse", { w_id: "hashed" });
+sh.shardCollection("cs4224.district", { d_w_id: 1, d_id: 1 });
+sh.shardCollection("cs4224.district-next-undelivered-id", { d_w_id: 1, d_id: 1 });
+sh.shardCollection("cs4224.customer", { c_w_id: 1, c_d_id: 1, c_id: 1 });
+sh.shardCollection("cs4224.order-order-line", { o_w_id: 1, o_d_id: 1, o_id: -1 });
+sh.shardCollection("cs4224.item", { i_id: "hashed" });
+sh.shardCollection("cs4224.stock", { s_w_id: 1, s_i_id: 1 });
 EOF
