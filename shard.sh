990e7fca4d6a9503f87decab60e1d74308418d4e
Louis Lai <hoangdung2506@gmail.com>
2017-11-17 10:41:20 +0800

    update shard key

--- shard.sh
+++ shard.sh
@@ -9,7 +9,7 @@ sh.shardCollection("cs4224.warehouse", { w_id: "hashed" });
 sh.shardCollection("cs4224.district", { d_w_id: 1, d_id: 1 });
 sh.shardCollection("cs4224.district-next-undelivered-id", { d_w_id: 1, d_id: 1 });
 sh.shardCollection("cs4224.customer", { c_w_id: 1, c_d_id: 1, c_id: 1 });
-sh.shardCollection("cs4224.order-order-line", { o_w_id: 1, o_d_id: 1, o_id: -1 });
+sh.shardCollection("cs4224.order-order-line", { o_w_id: 1, o_d_id: 1, o_id: 1 });
 sh.shardCollection("cs4224.item", { i_id: "hashed" });
 sh.shardCollection("cs4224.stock", { s_w_id: 1, s_i_id: 1 });
 EOF
